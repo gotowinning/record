fcbb4de43c2aa8c928b1ee7ef2ce0da64a6900ee
hh.chen <hh.chen>
2017-01-22 16:52:00 +0800

    add

---
+++ record/record/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  record
+//
+//  Created by Hui.Chen on 2017/1/22.
+//  Copyright © 2017年 Hui.Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ record/record/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  record
+//
+//  Created by Hui.Chen on 2017/1/22.
+//  Copyright © 2017年 Hui.Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
